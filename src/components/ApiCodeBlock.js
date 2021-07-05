20d0082a9d9eab330b4ad58a9d40ab2b3aa9da39
Dazorn96 <marcodz554@gmail.com>
2021-07-05 18:11:23 +0200

    Minor update

---
+++ src/components/ApiCodeBlock.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './ApiCodeBlock.module.css';
+
+export default function ApiCodeBlock(params) {
+  const items = [];
+
+  const getStyleByMethod = (method) => {
+    switch (method) {
+      case 'GET': return styles.getMethod;
+      case 'PUT': return styles.putMethod;
+      case 'POST': return styles.postMethod;
+      case 'DELETE': return styles.deleteMethod;
+    }
+  }
+
+  for (let i = 0; i < params.items.length; i++) {
+    const method = params.items[i].method;
+    const uri = params.items[i].uri;
+
+    items.push((
+      <div>
+        <span className={`${styles.method} ${getStyleByMethod(method)}`}>
+          {method}
+        </span>
+        {uri}
+      </div>
+    ));
+  }
+
+  return (
+    <div className={styles.block}>
+      <div className={styles.blockHead}>
+        {params.title}
+      </div>
+      <div className={styles.blockBody}>
+        {items}
+      </div>
+    </div>
+  );
+}
